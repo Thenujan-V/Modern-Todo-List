71ddc1d6bb9ef58f6a6252129e79e35020a11eea
vThenujan <vthenujan7400@gmail.com>
2023-08-23 09:41:39 +0530

    build to next level

--- odo.js
+++ odo.js
@@ -6,25 +6,28 @@ let Completed = document.getElementById('complete');
 let Clear = document.getElementById('clear');
 let add = document.getElementById("add");
 let first = document.querySelector("#first");
-let buttons = document.getElementById("buttons")
+let buttons = document.getElementById("buttons");
+let radio = document.querySelector(".inputRadio");
 
 
-
-add.addEventListener('click', () => {
-    let radio = document.createElement('input')
-    radio.setAttribute("type","radio")
-    list.appendChild(radio)
-    radio.style.width = '15px'
-   
-    
+radio.addEventListener('click', () => {
     addTodo(input.value);
     let line = document.createElement('hr')
     list.appendChild(line)
     input.value = ''
-    
+    radio.checked = false;
 })
 function addTodo(todo){
+    var x = document.createElement("INPUT");
+    x.type = "radio";
+    list.appendChild(x)
+    x.style.paddingTop = "200px";
+    x.style.width = "20px";
+    x.style.height = "20px";
     let para = document.createElement('p')
     para.innerText = todo
     list.appendChild(para)
+    x.style.position="absolute"
+    para.style.paddingLeft = "40px";
+    
 }
